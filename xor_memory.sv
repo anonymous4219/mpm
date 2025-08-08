97717221d8a88f754cc1d2e28ca707e8372df358
Kevin Townsend <kevintownsend4219@gmail.com>
2025-08-08 11:43:34 -0700

    Add log2 header file

--- xor_memory.sv
+++ xor_memory.sv
@@ -1,4 +1,13 @@
-module xor_memory(clk);
+`include "log2.vh"
+
+module xor_memory(clk, addr, d, en, q);
+  parameter WIDTH = 8;
+  parameter DEPTH = 256;
+  parameter PORTS = 2;
   input clk;
+  input [PORTS-1:0]addr[log2(DEPTH)-1:0];
+  input [PORTS-1:0]d[WIDTH-1:0];
+  input en[PORTS-1:0];
+  output [PORTS-1:0]q[WIDTH-1:0];
 
 endmodule
--- xor_memory_tb.sv
+++ xor_memory_tb.sv
@@ -1,19 +1,28 @@
+`include "log2.vh"
+
 module xor_memory_tb;
+  parameter WIDTH = 8;
+  parameter DEPTH = 256;
+  parameter PORTS = 2;
 
-logic clk;
+  logic clk;
+  logic [PORTS-1:0]addr[log2(DEPTH)-1:0];
+  logic [PORTS-1:0]d[WIDTH-1:0];
+  logic en[PORTS-1:0];
+  logic [PORTS-1:0]q[WIDTH-1:0];
 
-xor_memory dut(clk);
+  xor_memory #(WIDTH, DEPTH, PORTS) dut(clk, addr, d, en, q);
 
-initial begin
-  forever begin
-    clk = 0;
-    #10 clk = ~clk;
+  initial begin
+    forever begin
+      clk = 0;
+      #10 clk = ~clk;
+    end
   end
-end
 
-initial begin
-  #100 $display("Passed");
-  $finish();
-end
+  initial begin
+    #100 $display("Passed");
+    $finish();
+  end
 
 endmodule // xor_memory_tb
