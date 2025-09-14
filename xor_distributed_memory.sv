e0b498ee28ef5392622b0f603b15be95e1cf1692
Kevin Townsend <kevintownsend4219@gmail.com>
2025-09-14 13:14:15 -0700

    .

--- xor_distributed_memory.sv
+++ xor_distributed_memory.sv
@@ -1,7 +1,7 @@
 module xor_distributed_memory #(
-  parameter WIDTH = 8,
-  parameter DEPTH = 256,
-  parameter PORTS = 2)(
+  parameter WIDTH = 32,
+  parameter DEPTH = 512,
+  parameter PORTS = 32)(
 
   input clk,
   input [$clog2(DEPTH)-1:0]addr[PORTS-1:0],
