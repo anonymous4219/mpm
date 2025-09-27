2b1d1c66355c64d2751ee47cb969eeadf26b786b
Kevin Townsend <kevintownsend4219@gmail.com>
2025-09-27 09:18:05 -0700

    Added a write-first memory.

--- simple_dual_port_memory_wrapper.sv
+++ simple_dual_port_memory_wrapper.sv
@@ -1,7 +1,6 @@
 module simple_dual_port_memory_wrapper #(
   parameter WIDTH = 8,
-  parameter DEPTH = 256,
-  parameter WRITE_FIRST = 0)(
+  parameter DEPTH = 256)(
   input clk,
   input [$clog2(DEPTH)-1:0]addr_d,
   input [WIDTH-1:0]d,
@@ -11,17 +10,32 @@ module simple_dual_port_memory_wrapper #(
 
   reg [WIDTH-1:0]ram[DEPTH-1:0];
 
+  logic [WIDTH-1:0] sdp_q;
   simple_dual_port_memory #(
-    .WIDTH = WIDTH,
-    .DEPTH = DEPTH,
+    .WIDTH(WIDTH),
+    .DEPTH(DEPTH)
   )sdp(
     clk,
     addr_d,
     d,
     en,
     addr_q,
-    q
+    sdp_q
   );
-  // TODO: bypass on write first mode.
+
+  logic addr_eq;
+  logic [WIDTH-1:0]d_buffered;
+  always @(posedge clk) begin
+    addr_eq <= addr_d == addr_q;
+    d_buffered <= d;
+  end
+
+  always_comb begin
+    if(addr_eq) begin
+      q = d_buffered;
+    end else begin
+      q = sdp_q;
+    end
+  end
 
 endmodule // simple_dual_port_memory_wrapper
